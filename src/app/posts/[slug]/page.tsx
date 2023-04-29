547634fa7d0e7f3a25441466ec636eb941d2232f
Junho Kim <twinjyjh5@gmail.com>
2023-04-29 11:46:04 +0000

    Add Detail Page - Get md & Show React Markdown

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import ReactDom from "react-dom";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 type Props = {
   params: {
@@ -13,24 +16,53 @@ type Props = {
 
 export default function DetailPage({ params }: Props) {
   const [content, setContent] = useState("");
-  const markdownPath = `./${params.slug}.md`;
-  console.log(markdownPath)
 
-  
-  //import(markdownPath).then(response => {
-  //  return response.text()
-  //}).then(markdown => {
-  //  console.log(markdown)
-  //})
-  //import(`../../../../data/posts/${params.slug}.md`).then(res =>
-  //  console.log(res.text())
-  //);
+  useEffect(() => {
+    fetch(`/data/posts/${params.slug}.md`)
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => setContent(data));
+  }, []);
 
-  //const markdownData = require(`./data/posts/${params.slug}.md`)
   return (
     <>
-      <h1 className="font-bold text-center text-orange-600 text-2xl">{params.slug} 추후 제작 예정</h1>
-      {/* <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} /> */}
+      <div className="relative w-full h-80">
+        <Image
+          className="rounded-t-lg"
+          src={`/images/posts/${params.slug}.png`}
+          alt={`${params.slug}`}
+          fill
+        />
+      </div>
+      {content ? (
+        <div className="flex justify-center items-center">
+          <ReactMarkdown
+            className="prose"
+            children={content}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    {...props}
+                    children={String(children).replace(/\n$/, "")}
+                    style={a11yDark}
+                    language={match[1]}
+                    PreTag="div"
+                  />
+                ) : (
+                  <code {...props} className={className}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
